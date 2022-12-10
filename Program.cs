560c13fa0070f89ea1e8e2b4071f65ea0ce315b9
Martin Paquin <martin.paquin@gmail.com>
2022-12-10 12:37:34 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.ClearProviders();
+    builder.AddOpenTelemetry(options =>
+    {
+        options
+            .ConfigureResource(c => c.AddService("GettingStarted"))
+            .AddConsoleExporter()
+            .AddOtlpExporter(config =>
+            {
+                config.Endpoint = new Uri("http://localhost:4317");
+                config.Protocol = OtlpExportProtocol.HttpProtobuf;
+            });
+    });
+});
+
+var logger = loggerFactory.CreateLogger<Program>();
+
+for (var i = 0; i < 100; i++)
+{
+    logger.LogInformation("{count}: Hello World!", i);
+    await Task.Delay(1000);
+}
